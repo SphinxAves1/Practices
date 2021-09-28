47df43e5a341ea5e53a0789f233d61b2e2974411
SphinxAves1 <91510276+SphinxAves1@users.noreply.github.com>
2021-09-28 09:39:55 +0800

    sample strings

---
+++ LengthExample1.java
@@ -0,0 +1,10 @@
+public class LengthExample1 {
+public static void main(String [] args) {
+	String s1= "Good Day";
+	String s2="How are you?";
+	System.out.println("String Length is:" +s1.length());
+	System.out.println("String Length is:" +s2.length());
+	
+
+ }
+}
